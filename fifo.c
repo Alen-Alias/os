e4dccac7bde03a719ce310c7e568222e3f6e73ef
lazy-useR2 <alenaliasalukkal2004@gmail.com>
2025-03-31 20:41:16 +0530

    addded fifo new

--- fifo.c
+++ fifo.c
@@ -1,75 +1,101 @@
-#include <stdio.h>
-
-void fifo(int pages[], int n, int capacity) {
-    int frame[capacity];
-    int page_faults = 0;
-    int page_hits = 0;
-    float hit_ratio, miss_ratio;
-    
-    // Initialize frames with -1 to indicate empty
-    for(int i = 0; i < capacity; i++) {
-        frame[i] = -1;
+//Alen Alias, CSE A 25
+#include<stdio.h>
+void fifo(int pages[],int n,int nof)
+{
+    int page_faults=0,page_hits=0,i,j,k,flag=0;
+    int frame[nof],time[nof];
+    for(i=0;i<nof;i++)
+    {
+        frame[i]=-1;
+        time[i]=0;
     }
-    
-    // Index to track the oldest page in frame
-    int current = 0;
-    
-    // Process each page request
-    for(int i = 0; i < n; i++) {
-        int flag = 0;
-        
-        // Check if page already exists in frame
-        for(int j = 0; j < capacity; j++) {
-            if(frame[j] == pages[i]) {
-                flag = 1;
+    for(i=0;i<n;i++)
+    {
+        flag=0;
+        for(j=0;j<nof;j++)
+        {
+            if(frame[j]==pages[i])
+            {
+                flag=1;
                 page_hits++;
                 break;
             }
         }
-        
-        // If page not found, replace using FIFO
-        if(flag == 0) {
-            frame[current] = pages[i];
-            current = (current + 1) % capacity;
+        if(flag==0)
+        {
             page_faults++;
-            
-            // Display current frame state
-            printf("\nFor page %d: ", pages[i]);
-            for(int j = 0; j < capacity; j++) {
-                if(frame[j] == -1)
-                    printf("- ");
-                else
-                    printf("%d ", frame[j]);
+            for(j=0;j<nof;j++)
+            {
+                if(frame[j]==-1)
+                {
+                    frame[j]=pages[i];
+                    time[j]=i;
+                    flag=1;
+                    break;
+                }
+            }
+            if(flag==0)
+            {
+                k=0;
+                for(j=1;j<nof;j++)
+                {
+                    if(time[j]<time[k])
+                    {
+                        k=j;
+                    }
+                }
+                frame[k]=pages[i];
+                time[k]=i;
             }
         }
+        printf("Frame: ");
+        for(j=0;j<nof;j++)
+        {
+            printf("%d ",frame[j]);
+        }
+        printf("\n");
     }
-    
-    // Calculate ratios
-    hit_ratio = (float)page_hits / n;
-    miss_ratio = (float)page_faults / n;
-    
-    printf("\n\nTotal Page Faults: %d", page_faults);
-    printf("\nTotal Page Hits: %d", page_hits);
-    printf("\nHit Ratio: %.2f", hit_ratio);
-    printf("\nMiss Ratio: %.2f\n", miss_ratio);
+    printf("Total Page Faults: %d\n",page_faults);
+    printf("Total Page Hits: %d\n",page_hits);
 }
-
-int main() {
-    int n, capacity;
-    
-    printf("Enter number of pages: ");
-    scanf("%d", &n);
-    
-    int pages[n];
-    printf("Enter the page reference string: ");
-    for(int i = 0; i < n; i++) {
-        scanf("%d", &pages[i]);
-    }
-    
+void main()
+{
+    int pages[30],n,nof,i;
+    printf("Enter the number of pages: ");
+    scanf("%d",&n);
     printf("Enter the number of frames: ");
-    scanf("%d", &capacity);
-    
-    fifo(pages, n, capacity);
-    
-    return 0;
+    scanf("%d",&nof);
+    printf("Enter the pages: ");
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&pages[i]);
+    }
+    fifo(pages,n,nof);
 }
+/*
+Enter the number of pages: 20
+Enter the number of frames: 4
+Enter the pages: 7 0 1 2 0 3 0 4 2 3 0 3 2 1 2 0 1 7 0 1
+Frame: 7 -1 -1 -1 
+Frame: 7 0 -1 -1
+Frame: 7 0 1 -1
+Frame: 7 0 1 2
+Frame: 7 0 1 2
+Frame: 3 0 1 2
+Frame: 3 0 1 2
+Frame: 3 4 1 2
+Frame: 3 4 1 2
+Frame: 3 4 1 2
+Frame: 3 4 0 2
+Frame: 3 4 0 2
+Frame: 3 4 0 2
+Frame: 3 4 0 1
+Frame: 2 4 0 1
+Frame: 2 4 0 1
+Frame: 2 4 0 1
+Frame: 2 7 0 1
+Frame: 2 7 0 1
+Frame: 2 7 0 1
+Total Page Faults: 10
+Total Page Hits: 10
+*/
