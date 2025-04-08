69d8a9ceb161878725fcfeaf89747a10702a2496
lazy-useR2 <alenaliasalukkal2004@gmail.com>
2025-04-08 15:12:11 +0530

    Update bankers.c

--- bankers.c
+++ bankers.c
@@ -7,10 +7,10 @@ struct process
 void display(int avail[],int nop,int nor)
 {
     int i,j;
-    printf("\nprocess\tAlloc\tMax  \tavail\tNeed\n");
+    printf("\nprocess\tAlloc\t\tMax  \t\t  avail\t\tNeed\n");
     for(i=0;i<nop;i++)
     {
-        printf("P%d\t",i);
+        printf("%d\t",i);
         for(j=0;j<nor;j++)
             printf("%d ",p[i].alloc[j]);
         printf("\t");
@@ -30,64 +30,10 @@ void display(int avail[],int nop,int nor)
         printf("\n");
     }
 }
-void main()
+void issafe(int avail[],int nop,int nor)
 {
-    int i,j,k,flag=0,avail[5],req[5],nop,nor,safe[10],reqp,count=0;
-    printf("Enter the number of process : ");
-    scanf("%d",&nop);
-    printf("Enter the number of resourses : ");
-    scanf("%d",&nor);
-    for(i=0;i<nop;i++)
-    {
-        printf("Enter the resource allocated : ");
-        for(j=0;j<nor;j++)
-        {
-            scanf("%d",&p[i].alloc[j]);
-            p[i].status=0;
-        }
-        printf("Enter the max resource required : ");
-        for(j=0;j<nor;j++)
-        {
-            scanf("%d",&p[i].max[j]);
-            p[i].need[j]=p[i].max[j]-p[i].alloc[j];
-        }
-
-    }
-    printf("Enter the available resources : ");
-    for(i=0;i<nor;i++)
-    {
-        scanf("%d",&avail[i]);
-    }
-    display(avail,nop,nor);
-    printf("Enter the requesting process : ");
-    scanf("%d",&reqp);
-    printf("Enter the request : ");
-    for(i=0;i<nor;i++)
-    {
-        scanf("%d",&req[i]);
-    }
-    for(i=0;i<nor;i++)
-    {
-        if(req[i]>p[reqp].need[i])
-        {
-            printf("Error: Request is more than need\n");
-            return;
-        }
-        if(req[i]>avail[i])
-        {
-            printf("Error: Request is more than available\n");
-            return;
-        }
-    }
-    for(i=0;i<nor;i++)
-    {
-        avail[i]=avail[i]-req[i];
-        p[reqp].alloc[i]=p[reqp].alloc[i]+req[i];
-        p[reqp].need[i]=p[reqp].need[i]-req[i];
-    }
-    printf("After allocation\n");
-    display(avail,nop,nor);
-    for(int m=0;m<nop;m++)
+    int i,j,m,flag,k=0,count=0,safe[10];
+    for(m=0;m<nop;m++)
     {
     for(i=0;i<nop;i++)
     {
@@ -125,6 +71,64 @@ void main()
         for(i=0;i<nop;i++)
             printf("P%d ",safe[i]);
     }
+}
+void main()
+{
+    int i,j,avail[5],req[5],nop,nor,reqp;
+    printf("Enter the number of process : ");
+    scanf("%d",&nop);
+    printf("Enter the number of resourses : ");
+    scanf("%d",&nor);
+    for(i=0;i<nop;i++)
+    {
+        printf("Enter the resource allocated : ");
+        for(j=0;j<nor;j++)
+        {
+            scanf("%d",&p[i].alloc[j]);
+            p[i].status=0;
+        }
+        printf("Enter the max resource required : ");
+        for(j=0;j<nor;j++)
+        {
+            scanf("%d",&p[i].max[j]);
+            p[i].need[j]=p[i].max[j]-p[i].alloc[j];
+        }
 
-    
+    }
+    printf("Enter the available resources : ");
+    for(i=0;i<nor;i++)
+    {
+        scanf("%d",&avail[i]);
+    }
+    display(avail,nop,nor);
+    issafe(avail,nop,nor);
+    printf("\nEnter the requesting process : ");
+    scanf("%d",&reqp);
+    printf("Enter the request : ");
+    for(i=0;i<nor;i++)
+    {
+        scanf("%d",&req[i]);
+    }
+    for(i=0;i<nor;i++)
+    {
+        if(req[i]>p[reqp].need[i])
+        {
+            printf("Error: Request is more than need\n");
+            return;
+        }
+        if(req[i]>avail[i])
+        {
+            printf("Error: Request is more than available\n");
+            return;
+        }
+    }
+    for(i=0;i<nor;i++)
+    {
+        avail[i]=avail[i]-req[i];
+        p[reqp].alloc[i]=p[reqp].alloc[i]+req[i];
+        p[reqp].need[i]=p[reqp].need[i]-req[i];
+    }
+    printf("After allocation\n");
+    display(avail,nop,nor);
+    issafe(avail,nop,nor);
 }
